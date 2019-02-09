d4d24d2b7179658473f0eb89fabffe15409a1766
garyluf5 <gary.lu@f5.com>
2019-02-09 00:20:59 -0800

    Update provider.tf

--- HA_via_api/provider.tf
+++ HA_via_api/provider.tf
@@ -1,8 +1,8 @@
 # Configure the Microsoft Azure Provider, replace Service Principal and Subscription with your own
 provider "azurerm" {
-    subscription_id = "53392653-4d69-4753-b1e9-b7b2c270ffc6"
-    client_id       = "a61b6970-ea78-498a-9ee8-d746a2acc4df"
-    client_secret   = "BI9/b/7b+Eu3IE7UaY/vv3mZzRpX+ei9i7u9OlqdNGY="
-    tenant_id       = "e569f29e-b098-4cea-b6f0-48fa8532d64a"
+    subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    client_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    client_secret   = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    tenant_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
